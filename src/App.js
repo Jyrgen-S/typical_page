e2d82a39ef00f1a1feb139aec89db1020f579f71
jurgen <revisto.rourke@gmail.com>
2024-04-05 09:17:59 +0300

    User login and logout  with useContext hook

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 import Login from "./components/Login/Login";
 
+import AuthContext from "./Store/auth-context";
+
 function App() {
   const [loggedIn, setLoggedIn] = useState(() => {
-    if (JSON.parse(localStorage.getItem("isLoggedUser")) !== null) {
-      return JSON.parse(localStorage.getItem("isLoggedUser")).isLogged;
-    } else {
-      return false;
-    }
+    const storedLoggedUserData = JSON.parse(
+      localStorage.getItem("isLoggedUser")
+    );
+    return storedLoggedUserData ? storedLoggedUserData.isLogged : false;
   });
-
-  console.log(loggedIn);
 
   useEffect(() => {
     const storedLoggedUserData = JSON.parse(
       localStorage.getItem("isLoggedUser")
     );
-    if (storedLoggedUserData !== null) {
-      if (storedLoggedUserData.isLogged === true) {
-        setLoggedIn(true);
-      }
+    if (storedLoggedUserData && storedLoggedUserData.isLogged) {
+      setLoggedIn(true);
     }
   }, []);
 
   const loginHandler = (user, password) => {
-    const isLoggedUser = localStorage.setItem(
+    localStorage.setItem(
       "isLoggedUser",
       JSON.stringify({
         username: user,
@@ -40,14 +37,22 @@ function App() {
     localStorage.removeItem("isLoggedUser");
     setLoggedIn(false);
   };
+
   return (
-    <Fragment>
-      <MainHeader isAuthenticated={loggedIn} onLogout={logoutHandler} />
-      <main>
-        {!loggedIn && <Login onLogin={loginHandler} />}
-        {loggedIn && <Home />}
-      </main>
-    </Fragment>
+    <AuthContext.Provider
+      value={{
+        loggedIn: loggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
+      <Fragment>
+        <MainHeader />
+        <main>
+          {!loggedIn && <Login onLogin={loginHandler} />}
+          {loggedIn && <Home />}
+        </main>
+      </Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,23 +1,30 @@
+import React from "react";
 import "./Navigation.css";
 import Button from "../UI/Button";
 
+import AuthContext from "../../Store/auth-context";
+
 const Navigation = (props) => {
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <a href="/users">Users</a>
-        </li>
-        <li>
-          <a href="/admin">Admin</a>
-        </li>
-        {props.loggedIn && (
-          <li>
-            <Button onClick={props.onLogout}>Log out</Button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(context) => (
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="/users">Users</a>
+            </li>
+            <li>
+              <a href="/admin">Admin</a>
+            </li>
+            {props.loggedIn && (
+              <li>
+                <Button onClick={context.onLogout}>Log out</Button>
+              </li>
+            )}
+          </ul>
+        </nav>
+      )}
+    </AuthContext.Consumer>
   );
 };
 
